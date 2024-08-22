6388cea1abba47c12410773669de17bcbda23c85
Ajeet Singh Raina <ajeetraina@gmail.com>
2024-08-22 23:16:03 +0530

    using a single env file

--- seed.js
+++ seed.js
@@ -3,11 +3,11 @@ const { Pool } = require('pg');
 
 // Create a new pool using environment variables
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_DB_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
 });
 
 const seedData = async () => {
